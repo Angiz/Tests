c297ee465394ccff48d3c3e1b2e0a674e1d94e2a
crdiam <angiziewska@gmail.com>
2021-12-04 12:59:06 +0100

    removing test returnObject

--- src/test/java/pl/net/focus/ClassToTestTest.java
+++ src/test/java/pl/net/focus/ClassToTestTest.java
@@ -16,9 +16,4 @@ public class ClassToTestTest {
         assertEquals(22, classToTest.sum(2, 4*5));
         assertEquals(7, classToTest.sum(Integer.parseInt(""+'\u0035'), 2));
     }
-
-    @Test
-    public void returnObject() {
-        returnObject();
-    }
 }
